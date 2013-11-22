e282ba1eb66a0b4c9eed53331e8a9e8d9fd97757
Che-Liang Chiou <clchiou@gmail.com>
2013-11-22 01:53:52 -0800

    Add presubmit tests

--- env.sh
+++ env.sh
@@ -36,3 +36,35 @@ function t() {
   done
   test ${#PASSED[@]} -eq ${#UNITTESTS[@]}
 }
+
+
+# Run presubmit tests
+function presubmit() {
+  if [ -z "$1" ]; then
+    echo "Usage: presubmit file{.cc}"
+    return 1
+  fi
+
+  local PROGRAM="./${1%.cc}"
+  if [ ! -x "${PROGRAM}" ]; then
+    echo "${PROGRAM} is not an executable"
+    return 1
+  fi
+
+  local RET=0
+  local INPUT_PATH=
+  local OUTPUT_PATH=
+  for INPUT_PATH in *in*; do
+    OUTPUT_PATH="$(echo ${INPUT_PATH} | sed -e 's/in/out/')"
+    if ! ${PROGRAM} < ${INPUT_PATH} | diff -q ${OUTPUT_PATH} - ; then
+      ${PROGRAM} < ${INPUT_PATH} | diff -y ${OUTPUT_PATH} -
+      RET=1
+    fi
+  done
+
+  if [ "${RET}" -eq 0 ]; then
+    echo "All tests passed"
+  fi
+
+  return $RET
+}
