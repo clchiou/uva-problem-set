11e4c4b3133911755867d66513a1517c9e7ccf6d
Che-Liang Chiou <clchiou@gmail.com>
2013-11-25 21:57:42 -0800

    Solve: 102 - Ecological Bin Packing

---
+++ solved/102/102.cc
@@ -0,0 +1,113 @@
+#include <algorithm>
+#include <iostream>
+
+#ifdef ONLINE_JUDGE
+#define NDEBUG
+#endif
+#include <assert.h>
+
+enum Color {
+  BROWN,
+  GREEN,
+  CLEAR,
+};
+
+class Bin {
+ public:
+  Bin() : brown_(0), green_(0), clear_(0) {}
+  Bin(int brown, int green, int clear)
+      : brown_(brown), green_(green), clear_(clear) {}
+
+  friend std::istream &operator>>(std::istream &in, Bin &bin) {
+    in >> bin.brown_ >> bin.green_ >> bin.clear_;
+    return in;
+  }
+
+  int CountMoveOut(Color color) const {
+    int move = 0;
+    if (color != BROWN)
+      move += brown_;
+    if (color != GREEN)
+      move += green_;
+    if (color != CLEAR)
+      move += clear_;
+    return move;
+  }
+
+ private:
+  int brown_, green_, clear_;
+};
+
+int CountMove(const Bin bins[3], const Color order[3]) {
+  int move = 0;
+  for (int i = 0; i < 3; i++) {
+    move += bins[i].CountMoveOut(order[i]);
+  }
+  return move;
+}
+
+void FindMinMove(const Bin bins[3], int *min_move_out, Color min_order[3]) {
+  // Alphabetical order.
+  const Color all_colors[] = { BROWN, CLEAR, GREEN };
+  const int permutations[][3] = {
+    { 0, 1, 2 },
+    { 0, 2, 1 },
+    { 1, 0, 2 },
+    { 1, 2, 0 },
+    { 2, 0, 1 },
+    { 2, 1, 0 },
+  };
+  const int size = sizeof(permutations) / sizeof(permutations[0]);
+
+  const int *min_p = NULL;
+  int min_move = 0;
+  for (int i = 0; i < size; i++) {
+    const Color order[3] = {
+      all_colors[permutations[i][0]],
+      all_colors[permutations[i][1]],
+      all_colors[permutations[i][2]],
+    };
+    int move = CountMove(bins, order);
+    if (!min_p || move < min_move) {
+      min_p = permutations[i];
+      min_move = move;
+    }
+  }
+
+  for (int j = 0; j < 3; j++) {
+    min_order[j] = all_colors[min_p[j]];
+  }
+  *min_move_out = min_move;
+}
+
+char ToChar(Color color) {
+  switch (color) {
+    case BROWN:
+      return 'B';
+    case GREEN:
+      return 'G';
+    case CLEAR:
+      return 'C';
+  }
+  assert(0);
+  return '\0';
+}
+
+int main() {
+  Bin bins[3];
+
+  while (std::cin >> bins[0] >> bins[1] >> bins[2]) {
+    int min_move = 0;
+    Color min_order[3];
+    FindMinMove(bins, &min_move, min_order);
+    std::cout
+      << ToChar(min_order[0])
+      << ToChar(min_order[1])
+      << ToChar(min_order[2])
+      << ' '
+      << min_move
+      << std::endl;
+  }
+
+  return 0;
+}
---
+++ solved/102/102_unittest.cc
@@ -0,0 +1,77 @@
+#define main _main
+#include "102.cc"
+
+#include <sstream>
+
+#include "gtest/gtest.h"
+
+#include "test_fixtures.h"
+
+TEST(Test102, TestCountMoveOut) {
+  Bin bin(3, 4, 5);
+  ASSERT_EQ(bin.CountMoveOut(BROWN), 4 + 5);
+  ASSERT_EQ(bin.CountMoveOut(GREEN), 3 + 5);
+  ASSERT_EQ(bin.CountMoveOut(CLEAR), 3 + 4);
+}
+
+TEST(Test102, TestCountMove) {
+  Bin bins[3] = {
+    Bin(  3,   4,   5),
+    Bin( 30,  40,  50),
+    Bin(300, 400, 500),
+  };
+  Color order[3];
+
+  order[0] = BROWN;
+  order[1] = GREEN;
+  order[2] = CLEAR;
+  ASSERT_EQ(CountMove(bins, order), 4 + 5 + 30 + 50 + 300 + 400);
+
+  order[0] = CLEAR;
+  order[1] = GREEN;
+  order[2] = BROWN;
+  ASSERT_EQ(CountMove(bins, order), 3 + 4 + 30 + 50 + 400 + 500);
+
+  order[0] = GREEN;
+  order[1] = CLEAR;
+  order[2] = BROWN;
+  ASSERT_EQ(CountMove(bins, order), 3 + 5 + 30 + 40 + 400 + 500);
+}
+
+static void Compare(const Bin bins[3],
+    int min_move, const std::string &min_order) {
+  int min_move_out = 0;
+  Color min_order_out[3];
+  FindMinMove(bins, &min_move_out, min_order_out);
+  ASSERT_EQ(min_move, min_move_out);
+  ASSERT_EQ(min_order[0], ToChar(min_order_out[0]));
+  ASSERT_EQ(min_order[1], ToChar(min_order_out[1]));
+  ASSERT_EQ(min_order[2], ToChar(min_order_out[2]));
+}
+
+TEST(Test102, TestFindMinMove) {
+  Bin bins[3];
+
+  bins[0] = Bin(  3,   4,   5);
+  bins[1] = Bin( 30,  40,  50);
+  bins[2] = Bin(300, 400, 500);
+  Compare(bins, 4 + 5 + 30 + 50 + 300 + 400, "BGC");
+
+  bins[0] = Bin(  1, 100,   1);
+  bins[1] = Bin(  1,   1, 100);
+  bins[2] = Bin(100,   1,   1);
+  Compare(bins, 6, "GCB");
+}
+
+TEST(Test102, TestFindMinMoveAlphabetically) {
+  Bin bins[3];
+
+  bins[0] = Bin(1, 1, 1);
+  bins[1] = Bin(1, 1, 1);
+  bins[2] = Bin(1, 1, 1);
+  Compare(bins, 6, "BCG");
+}
+
+TEST_F(IntegrationTest, TestSampleData) {
+  RunTestData(_main, "fin1", "fout1");
+}
